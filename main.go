bbff4409c6155734520e7c57579f75129854d5aa
itokun12 <naoya.ito@smartdrive.co.jp>
2017-11-12 21:05:22 +0900

    Set routing and Success upload

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	"github.com/Sirupsen/logrus"
-	"os"
+	"github.com/gin-gonic/gin"
 	"github.com/itokun12/face-maker/models"
+	"github.com/joho/godotenv"
+	"net/http"
+	"os"
+	"log"
+	"io"
 )
 
 func main() {
@@ -15,6 +18,33 @@ func main() {
 	}
 	r := gin.Default()
 	models.Initialize()
+	r.LoadHTMLGlob("views/parts/*")
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/parts")
+	})
+	r.GET("/parts", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Face-Maker",
+		})
+	})
+	r.GET("/parts/new", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "new.tmpl", gin.H{
+			"title": "Face-Maker",
+		})
+	})
+	r.POST("/parts", func(c *gin.Context) {
+		file, header, err := c.Request.FormFile("file")
+		filename := header.Filename
+		out, err := os.Create("./images/"+filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+		_, err = io.Copy(out, file)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
 	r.Run(":" + listenPort())
 }
 
